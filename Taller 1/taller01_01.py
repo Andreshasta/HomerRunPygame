6092d467f93cc1def0bc980548db24c75ed7b34c
Andreshasta <andresmhasta@gmail.com>
2017-07-03 01:15:18 -0500

    Reorganización

--- Taller 1/taller01_01.py
+++ Taller 1/taller01_01.py
@@ -1,7 +1,7 @@
 import sys, pygame
 
 size = width, height = 640, 400
-speed = [1, 1]
+speed = [0, 2]
 screen = pygame.display.set_mode(size)
 
 def main():
@@ -10,24 +10,20 @@ def main():
         ballrect = ball.get_rect()
         ballrect.left = (width/2)-(ballrect.width/2)
         ballrect.top = (height/2)-(ballrect.height/2)
-
         court = pygame.image.load("imagenes/cancha.png")
         courtrect = court.get_rect()
-
         while 1:
                 for event in pygame.event.get():
                         if event.type == pygame.QUIT:
                                 sys.exit()
                 ballrect = ballrect.move(speed)
                 if ballrect.top < height-height or ballrect.bottom > height:
-                
-                    speed[1] = -speed[1]
-                if  ballrect.left < width-width or ballrect.right > width :
-                    speed[0] = -speed[0]
+                        speed[1] = -speed[1]
+
                 screen.blit(court, courtrect)
                 screen.blit(ball, ballrect)
 
                 pygame.display.update()
 
 if __name__ == '__main__':
- main()
+        main()
